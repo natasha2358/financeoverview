ebd7d6d5922cc94fd1dcdf9d9ce0f7a49aee9bcc
natasha2358 <37518715+natasha2358@users.noreply.github.com>
2026-02-08 21:46:06 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 53397,
+    port: 5173,
     proxy: {
-      "/api": "http://localhost:5000"
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false
+      }
     }
   }
 });
